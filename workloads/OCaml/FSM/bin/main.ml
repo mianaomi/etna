5d99b23f013577ab7b54755c158f13873e9ff39e
mianaomi <miacohen.inbox@gmail.com>
2024-06-03 14:47:18 -0400

    ..

--- workloads/OCaml/FSM/bin/main.ml
+++ workloads/OCaml/FSM/bin/main.ml
@@ -1,3 +1,9 @@
 open QCheck
+open Util.Runner
+open Util.Io
+open FSM.Impl
+open FSM.Test
+open FSM.QcheckType
+open FSM.QcheckBespoke
 
 let () = print_endline "Hello, World!"
--- workloads/OCaml/FSM/lib/impl.ml
+++ workloads/OCaml/FSM/lib/impl.ml
@@ -1,4 +1,5 @@
-type transition = int * char option * int
+type transition = int * char option * int 
+
 
 type fsm_t = {
   sigma: char list;
@@ -6,7 +7,8 @@ type fsm_t = {
   q0: int;
   fs: int list;
   delta: transition list;
-}
+} 
+
 
 let rec elem x a =
   match a with
@@ -25,12 +27,22 @@ let rec union a b =
      | [] -> [])
 
 
-     let rec subset a b =
-      match a with
-      | h::t -> (elem h b) && (subset t b)
-      | [] -> true
+let rec subset a b =
+  match a with
+    | h::t -> (elem h b) && (subset t b)
+    | [] -> true
     
-    let rec eq a b = (subset a b) && (subset b a)
+let rec eq a b = (subset a b) && (subset b a)
+
+let rec intersection a b =
+  match a with
+    | h::t -> if elem h b then insert h (intersection t b) else (intersection t b)
+    | [] -> []
+let explode (s: string) : char list =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l)
+  in
+  exp (String.length s - 1) []
 
 let move (fsm: fsm_t) (qs: int list) (s: char option) : int list =
   List.fold_left (fun acc x -> 
@@ -50,3 +62,12 @@ let rec e_closure (fsm: fsm_t) (qs: int list) : int list =
       then qs 
       else e_closure fsm next_qs
                 
+let accept (fsm: fsm_t) (s: string) : bool =
+  let rec aux states input =
+    match input with
+    | [] -> (match (intersection fsm.fs (e_closure fsm states)) with
+            |[] -> false
+            |h::t-> true)
+    | c :: cs -> aux (e_closure fsm (move fsm (e_closure fsm states) (Some c))) cs
+  in
+  aux [fsm.q0] (explode s)
