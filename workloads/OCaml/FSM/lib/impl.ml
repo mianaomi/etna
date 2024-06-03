739d178b56cc1075a7baf00d24e535f5e78da18f
mianaomi <miacohen.inbox@gmail.com>
2024-06-03 18:54:36 -0400

    p3 code/versioning stuff

--- workloads/OCaml/FSM/lib/impl.ml
+++ workloads/OCaml/FSM/lib/impl.ml
@@ -1,13 +1,12 @@
-type transition = int * char option * int 
+type ('q) transition = 'q * char option * 'q [@@deriving sexp_of, quickcheck]
 
-
-type fsm_t = {
+type ('q) fsm_t = {
   sigma: char list;
-  qs: int list;
-  q0: int;
-  fs: int list;
-  delta: transition list;
-} 
+  qs: 'q list;
+  q0: 'q;
+  fs: 'q list;
+  delta: ('q) transition list;
+} [@@deriving sexp_of, quickcheck]
 
 
 let rec elem x a =
@@ -71,3 +70,67 @@ let accept (fsm: fsm_t) (s: string) : bool =
     | c :: cs -> aux (e_closure fsm (move fsm (e_closure fsm states) (Some c))) cs
   in
   aux [fsm.q0] (explode s)
+
+
+let new_states (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
+  List.fold_right (fun sym acc ->
+      let dest = e_closure nfa (move nfa qs (Some sym)) in
+      if dest = [] then []::acc else dest::acc
+  ) nfa.sigma []
+
+            
+let new_trans (nfa: ('q,'s) nfa_t) (qs: 'q list): ('q list,'s) transition list =
+ List.fold_right (fun sym acc -> 
+                      (qs, Some sym, (e_closure 
+                                          nfa 
+                                          (move 
+                                              nfa 
+                                              qs 
+                                              (Some sym)
+                                          )
+                                  )
+                        )::acc
+                ) 
+                nfa.sigma
+                [] 
+                
+                   
+                   
+let new_finals (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
+if List.exists (fun q -> List.mem q nfa.fs) qs then [qs] else []
+  let nfa_to_dfa_step (nfa: int fsm_t) (dfa: int list fsm_t) (worklist: int list list) =
+    match worklist with
+    | [] -> (dfa, [])
+    | qs :: rest ->
+      let states = new_states nfa qs in  
+      let transitions = new_trans nfa qs in  
+      let finals = new_finals nfa qs in 
+        let updated_dfa = {
+          sigma = dfa.sigma;
+          qs = Sets.union dfa.qs states;
+          q0 = dfa.q0;
+          fs = Sets.union dfa.fs finals;
+          delta = Sets.union dfa.delta transitions;
+      } in
+      
+      let new_worklist = Sets.union rest (Sets.minus states (List.map (fun (t,a,b) -> t) dfa.delta)) in
+      (updated_dfa, new_worklist)
+
+  let nfa_to_dfa (nfa: (int) fsm_t) : (int list) fsm_t =
+    let initial_state = e_closure nfa [nfa.q0] in  
+    let initial_dfa = {
+      sigma = nfa.sigma;
+      qs = [initial_state];
+      q0 = initial_state;
+      fs = if List.exists (fun q -> List.mem q nfa.fs) initial_state then [initial_state] else [];
+      delta = [];
+    } in
+    
+    let rec loop dfa worklist =
+      match worklist with
+      | [] -> dfa  
+      | _ ->
+        let (updated_dfa, new_worklist) = nfa_to_dfa_step nfa dfa worklist in
+        loop updated_dfa new_worklist
+    in
+    loop initial_dfa [initial_state] 
