37b8e8704485fddbdef2351d4113520d8d08b701
mianaomi <miacohen.inbox@gmail.com>
2024-06-04 18:48:38 -0400

    spec skel

--- workloads/OCaml/FSM/lib/spec.ml
+++ workloads/OCaml/FSM/lib/spec.ml
@@ -1,3 +1,36 @@
 open Impl
 open List
 open Util.Runner
+
+(* Additional Functions *)
+
+let rec ( === ) fsm_a fsm_b = failwith("not added")
+
+let is_functional_FSM = failwith("not added")
+
+(* -- Validity Properties. *)
+
+let prop_moveValid = failwith("not added")
+
+(* ---------- *)
+
+(* -- Postcondition Properties. *)
+
+let prop_movePost = failwith("not added")
+(*Assuming that we have a functional FSM, 
+move(FSM, state)’s output should contain no states 
+that are not in the machine’s state list.*)
+let prop_acceptPost = failwith("not added")
+(*Assuming that we have functional FSMs, 
+if FSM1 = FSM2 then accept(FSM1, a string) 
+  should be the same as accept(FSM2, the same string)*)
+let prop_conversionPost = failwith("not added")
+(*Assuming that we have a functional FSM, 
+accept(FSM, a string) should be the same 
+as accept(nfa_to_dfa(FSM,  the same string))*)
+
+
+
+
+
+
