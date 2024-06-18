6aa5f5dbb761624b7c21d830a8ba012a11afcbef
mianaomi <miacohen.inbox@gmail.com>
2024-06-18 12:44:16 -0400

    git test/impl.ml

--- workloads/OCaml/FSM/lib/spec.ml
+++ workloads/OCaml/FSM/lib/spec.ml
@@ -57,6 +57,15 @@ let is_functional_FSM (fsm : ('q) fsm_t) : bool =
 
 (* -- Postcondition Properties. *)
 
+let prop_conversionPost: ('q) fsm_t * string -> test = 
+  fun (fsm, s) -> is_functional_FSM fsm 
+  ->> ((Impl.accept fsm s) = (Impl.accept (Impl.nfa_to_dfa(fsm)) s))
+(*Assuming that we have a functional FSM, 
+accept(FSM, a string) should be the same 
+as accept(nfa_to_dfa(FSM,  the same string))*)
+
+(*
+Postponed
 let prop_movePost: ('q) fsm_t * 'q list  * char -> test = 
   fun (fsm, sl, c) -> is_functional_FSM fsm ->> 
   let result_states = Impl.move fsm sl (Some c) in
@@ -73,12 +82,7 @@ let prop_acceptPost: ('q) fsm_t * ('q) fsm_t * string =
 if FSM1 = FSM2 then accept(FSM1, a string) 
 should be the same as accept(FSM2, the same string)*)
 
-let prop_conversionPost: ('q) fsm_t * string -> test = 
-  fun (fsm, s) -> is_functional_FSM fsm 
-  ->> ((Impl.accept fsm s) = (Impl.accept (Impl.nfa_to_dfa(fsm)) s))
-(*Assuming that we have a functional FSM, 
-accept(FSM, a string) should be the same 
-as accept(nfa_to_dfa(FSM,  the same string))*)
+*)
 
 
 
