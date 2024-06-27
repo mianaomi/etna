a6e05a0e1cbad7aa134a2c82fa27694f5605d327
mianaomi <miacohen.inbox@gmail.com>
2024-06-26 22:40:56 -0400

    qtype

--- workloads/OCaml/FSM/lib/Strategies/qcheckType.ml
+++ workloads/OCaml/FSM/lib/Strategies/qcheckType.ml
@@ -1,5 +1,62 @@
 open Impl
 
 module QcheckType = struct
+  let typebased =
+    let open QCheck.Gen in
+    let nfa_gen n =
+      if n <= 0 then return ([], [], 0, [], [])
+      else
+        (*simplify gen*)
 
+       (*very list sizes*) 
+        let gen_size = int_range 0 n in
+
+        let gen_sigma = (*gen alphabet length s, generate char list of length s*) 
+          gen_size >>= fun sigma_size ->
+          list_size (return sigma_size) char
+        in
+        let gen_states = (*gen number of states n, generate state list of length n*)
+          gen_size >>= fun states_size ->
+          list_size (return states_size) nat
+        in
+        let gen_initial_state = (*gen initial state*)
+          nat
+        in
+        let gen_final_states = (*gen number of states n, generate final state list of length n*)
+          gen_size >>= fun final_states_size ->
+          list_size (return final_states_size) nat
+        in
+        let gen_transition =
+          nat >>= fun from ->
+          nat >>= fun to_ ->
+          frequency [
+            (1, return (from, None, to_));
+            (4, char >>= fun c -> return (from, Some c, to_))
+          ]
+        in
+        let gen_transitions =
+          gen_size >>= fun transitions_size ->
+          list_size (return transitions_size) gen_transition
+        in
+
+        gen_sigma >>= fun sigma ->
+        gen_states >>= fun qs ->
+        gen_initial_state >>= fun q0 ->
+        gen_final_states >>= fun fs ->
+        gen_transitions >>= fun delta ->
+        return (sigma, qs, q0, fs, delta)
+    in
+    sized (fun n -> nfa_gen n)
+       (*
+  sigma: generate a number x, then add a new char x times
+  qs: generate a number x, then add a new int x times;
+  q0: generate a number x;
+  fs: generate a number x, then add a new int x times;
+  delta:generate a number x, then add a new transiton x times
+          for each transition: generate 2 ints, 
+          and 1/5 option = none 4/5 option = some char
+          transition = (int1, option, int2);
+    *)
+  
+  let qcheck_type = QCheck.make typebased
 end
